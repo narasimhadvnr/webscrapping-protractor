cb87dd264ebb28effbde3ad49353aa8aa3645b16
venkat <narasimha.dvnr@gmail.com>
2018-02-05 22:27:34 +0530

    added entire page code extraction

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,9 +7,19 @@ describe('webscrapping-protractor App', () => {
     page = new AppPage();
   });
 
+//   it('should click viewsource tab elements', () => {
+//     page.navigateTo('https://www.telerik.com/kendo-angular-ui/components/grid/scroll-modes/');
+//  //   expect(page.getParagraphText()).toEqual('Welcome to app!');
+//     page.sleepBrowser(3000);
+//   });
+
   it('should display welcome message', () => {
-    page.navigateTo();
+   page.navigateTo('https://www.telerik.com/kendo-angular-ui/components/grid/scroll-modes/');
+//   page.clickViewSourceElements();
+   
  //   expect(page.getParagraphText()).toEqual('Welcome to app!');
-    page.gettabstripElement();
+    page.gettabstripElement('scroll_modes');
+
+    page.sleepBrowser(20000);
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -2,36 +2,66 @@ import { browser, by, element, protractor, ExpectedConditions } from 'protractor
 import { ElementArrayFinder } from 'protractor/built/element';
 import { Utilities } from './utilities';
 const fs = require('fs');
+const mkdirp = require('mkdirp');
+
 
 export class AppPage {
-  navigateTo() {
+  navigateTo(url) {
     browser.waitForAngularEnabled(false);
-    return browser.get('https://www.telerik.com/kendo-angular-ui/components/grid/scroll-modes/');
+    return browser.get(url);
   }
 
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
 
-  gettabstripElement() {
 
-    element.all(by.css('.file-list')).then(function(items) {
+  clickViewSourceElements() {
+    element.all(by.css('a')).then(function (items) {
       for (let i = 0; i < items.length; i++) {
-        items[i].all(by.css('.docs-tabstrip li a')).then( (fileNames) => {
-          for (let anchor = 0; anchor < fileNames.length; anchor ++) {
-          fileNames[anchor].getText().then(result => {
-            if (result.length > 0) {
-              fileNames[anchor].click().then(() => {});
-              browser.sleep(2000);
-               items[i].element(by.css('.tab-content')).getText().then( (code) => {
-                console.log(result + ' ', code);
-               });
-            }
-          });
+        items[i].getText().then((text) => {
+          // console.log('text: ', text);
+          if (text.trim().toLowerCase() === 'view source') {
+            browser.actions().mouseMove( items[i]).click();
+            browser.actions().mouseDown().mouseUp().perform();
+            console.log('item found with view source');
           }
         });
       }
-      browser.sleep(5000);
+    });
+  }
+
+  sleepBrowser(duration) {
+    browser.sleep(duration);
+  }
+
+  gettabstripElement(componentName) {
+    const FOLDER_NAME = 'code_';
+    element.all(by.css('.file-list')).then(function(items) {
+      for (let i = 0; i < items.length; i++) {
+        const folderName = __dirname + '/' + componentName + '/' + FOLDER_NAME + i;
+        mkdirp(folderName, () => {
+          items[i].all(by.css('.docs-tabstrip li a')).then( (fileNames) => {
+            for (let anchor = 0; anchor < fileNames.length; anchor ++) {
+            fileNames[anchor].getText().then(result => {
+              if (result.length > 0) {
+                // browser.actions().mouseMove(fileNames[anchor]).click();
+                fileNames[anchor].click().then(() => {
+                  
+                });
+                browser.actions().mouseDown().mouseUp().perform();
+                // browser.sleep(2000);
+                 items[i].element(by.css('.tab-content')).getText().then( (code) => {
+                  const filePath = folderName + '/' + result;
+                  console.log('filePath: ', filePath, + ' code: ' + code);
+                  // fs.writeFileSync(filePath, code, 'utf-8');
+                 });
+              }
+            });
+            }
+          });
+        });
+      }
     });
 
   }
